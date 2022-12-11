44c7ddfab13f821c2e978efa90efc6dbc9421ac3
Daca <64079718+DanielCaldes@users.noreply.github.com>
2022-12-11 20:34:46 +0100

    FulletoEmbestidor

--- Topolino/Assets/Scripts/Fulletos/Detection.cs
+++ Topolino/Assets/Scripts/Fulletos/Detection.cs
@@ -2,16 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum EnemyType
+{
+    Embestidor,Prefab
+}
+
 public class Detection : MonoBehaviour
 {
+    public EnemyType enemyType;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
-            transform.parent.GetComponent<EnemyController>().PlayerDetected();
+        {
+            switch (enemyType)
+            {
+                case EnemyType.Prefab:
+                    transform.parent.GetComponent<EnemyController>().PlayerDetected();
+                    Debug.Log("Detectado Prefab!!");
+                    break;
+
+                case EnemyType.Embestidor:
+                    Debug.Log("Detectado Embestidor!!");
+                    transform.parent.GetComponent<EmbestidorController>().PlayerDetected();
+                    break;
+            }
+        }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
-            transform.parent.GetComponent<EnemyController>().PlayerLosed();
+        {
+            switch (enemyType)
+            {
+                case EnemyType.Prefab:
+                    transform.parent.GetComponent<EnemyController>().PlayerLosed();
+                    break;
+
+                case EnemyType.Embestidor:
+                    transform.parent.GetComponent<EmbestidorController>().PlayerLosed();
+                    break;
+            }
+        }
     }
 }
